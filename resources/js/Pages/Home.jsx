fc26f4c964022f9066fd555d667fa4310daceb4c
Kasparas <kas.galdikas@kvkedu.lt>
2024-12-19 15:02:51 +0200

    Your commit message

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -16,6 +16,7 @@ export default function Home() {
             {/* Hero Section */}
             <header className="hero">
                 <div className="hero-content">
+                <h1 className="hero-title">GIT</h1>
                     <h1 className="hero-title">The people platform—Where interests become friendships</h1>
                     <p className="hero-description">
                         Discover events, groups, and activities around you. Join now to connect with like-minded people.
