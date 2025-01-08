a0f14bfbac2bc4261a0161c101af482611905751
Kasparas Galdikas <kgaldikas123@gmail.com>
2025-01-08 18:51:27 +0200

    Add updated index.php to public directory

--- public/index.php
+++ public/index.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Http\Request;
 
+/*
+|--------------------------------------------------------------------------
+| Turn On The Lights
+|--------------------------------------------------------------------------
+|
+| In this file, we will load the Laravel framework and turn on the lights
+| for this application. Then, we can register the auto-loader, load
+| the application, and send the response back to the browser.
+|
+*/
+
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
@@ -12,6 +23,25 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request
+| through the kernel, and then send the response back to the
+| client's browser as a typical Laravel response.
+|
+*/
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
