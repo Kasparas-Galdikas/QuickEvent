6f86acda7a3c72e0bb77c607aebead3e3fd3cbbe
Kasparas Galdikas <kgaldikas123@gmail.com>
2025-01-09 14:39:32 +0200

    Truing to put the groups detail page into production

---
+++ public/build/assets/ApplicationLogo-D2KqXTyV.js
@@ -0,0 +1,2 @@
+import{j as t}from"./app-J_DYpR6M.js";function r(o){return t.jsx("img",{...o,src:"/images/logo1.png",alt:"Logo"})}export{r as A};
+//# sourceMappingURL=ApplicationLogo-D2KqXTyV.js.map
--- public/build/assets/ApplicationLogo-Db5I6lX8.js
+++
@@ -1,2 +0,0 @@
-import{j as t}from"./app-DzDXpD_K.js";function r(o){return t.jsx("img",{...o,src:"/images/logo1.png",alt:"Logo"})}export{r as A};
-//# sourceMappingURL=ApplicationLogo-Db5I6lX8.js.map
