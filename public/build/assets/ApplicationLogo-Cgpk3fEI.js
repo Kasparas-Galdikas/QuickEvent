7aeba01410ab344e04b856a8685ab06f5ba6ae3e
Kasparas Galdikas <kgaldikas123@gmail.com>
2025-01-10 11:31:05 +0200

    Added Edgaras changes for groups details page in production

---
+++ public/build/assets/ApplicationLogo-Cgpk3fEI.js
@@ -0,0 +1,2 @@
+import{j as t}from"./app-Bh-16c3K.js";function r(o){return t.jsx("img",{...o,src:"/images/logo1.png",alt:"Logo"})}export{r as A};
+//# sourceMappingURL=ApplicationLogo-Cgpk3fEI.js.map
