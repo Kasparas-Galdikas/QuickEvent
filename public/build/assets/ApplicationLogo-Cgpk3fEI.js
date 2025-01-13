1ff4fd2070d532fa70c4e8a533ad050757678170
Kasparas Galdikas <kgaldikas123@gmail.com>
2025-01-13 20:34:23 +0200

    Your commit message here

--- public/build/assets/ApplicationLogo-Cgpk3fEI.js
+++
@@ -1,2 +0,0 @@
-import{j as t}from"./app-Bh-16c3K.js";function r(o){return t.jsx("img",{...o,src:"/images/logo1.png",alt:"Logo"})}export{r as A};
-//# sourceMappingURL=ApplicationLogo-Cgpk3fEI.js.map
---
+++ public/build/assets/ApplicationLogo-DZ5gHQFH.js
@@ -0,0 +1,2 @@
+import{j as t}from"./app-CnRocTxr.js";function r(o){return t.jsx("img",{...o,src:"/images/logo1.png",alt:"Logo"})}export{r as A};
+//# sourceMappingURL=ApplicationLogo-DZ5gHQFH.js.map
