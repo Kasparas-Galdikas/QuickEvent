7941fb26f267a42f73ae9fbea9b191ef7c3a31a9
Kasparas Galdikas <kgaldikas123@gmail.com>
2025-01-15 19:22:34 +0200

    Build to production

--- public/build/assets/ApplicationLogo-DZ5gHQFH.js
+++
@@ -1,2 +0,0 @@
-import{j as t}from"./app-CnRocTxr.js";function r(o){return t.jsx("img",{...o,src:"/images/logo1.png",alt:"Logo"})}export{r as A};
-//# sourceMappingURL=ApplicationLogo-DZ5gHQFH.js.map
