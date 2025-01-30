9c37d76d1141d2a0d22670ce40b5ce5dafbda416
Kasparas Galdikas <kgaldikas123@gmail.com>
2025-01-30 14:53:31 +0200

    Minor style fixes

--- public/build/assets/ApplicationLogo-Dj7T7dkk.js
+++
@@ -1,2 +0,0 @@
-import{j as t}from"./app-DxeR9onv.js";function r(o){return t.jsx("img",{...o,src:"/images/logo1.png",alt:"Logo"})}export{r as A};
-//# sourceMappingURL=ApplicationLogo-Dj7T7dkk.js.map
--- public/build/assets/InputLabel-BpXJPpk_.js
+++
@@ -1,2 +0,0 @@
-import{j as m}from"./app-DxeR9onv.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block text-sm font-medium text-gray-700 "+e,children:t||r})}export{o as I};
-//# sourceMappingURL=InputLabel-BpXJPpk_.js.map
